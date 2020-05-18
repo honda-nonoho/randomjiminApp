9da06e1cd28044efb58905fcfaf0c27b0852c3a0
honda-nonoho <nnhhnd3012@gmail.com>
2020-05-19 00:50:29 +0900

    作った

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -1,6 +1,7 @@
 //
 //  ViewController.swift
-//  testApp
+//  jiminApp
+//
 //
 //  Created by nonoho.honda on 2020/05/18.
 //  Copyright © 2020 nonoho.honda. All rights reserved.
@@ -14,7 +15,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    //じゃんけん（数字）
+    var answerNumber = 0
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        
+        
+        
+        //新しいじゃんけんの結果を出すが、前回の結果と異なる
+        var newAnswerNumber = 0
+        
+        //ランダムに結果を出すが、前回の結果と異なる
+        //repeat は繰り返しを意味する
+        repeat {
+            
+            //0,1,2の数値をランダムに算出（乱数）
+            newAnswerNumber = Int.random(in: 0..<3)
+            
+            //前回と同じ結果のときは、再度、ランダム
+            //異なる結果のときは、repeat
+        } while answerNumber == newAnswerNumber
+        
+        //新しいじゃんけんの結果を格納
+        answerNumber = newAnswerNumber
+        
+       if answerNumber == 0 {
+        
+          //グー
+          answerLabel.text = "ジミン１"
+          answerImageView.image = UIImage(named: "jimin1")
+        
+       } else if answerNumber == 1 {
+        
+          //チョキ
+          answerLabel.text = "ジミン２"
+          answerImageView.image = UIImage(named: "jimin2")
+        
+       } else if answerNumber == 2 {
+        
+          //パー
+          answerLabel.text = "ジミン３"
+          answerImageView.image = UIImage(named: "jimin3")
+         
+    }
+  
+    }
 }
-
